8da30acf78504d06e1f73b7461f867c38a4eed1e
yogeshg29 <guptayogesh0123@gmail.com>
2026-01-04 23:57:51 +0530

    Add files via upload
    the herosection.jsx  and navigation.jsx are in componets and the there are images they put in public folder

---
+++ HeroSection.jsx
@@ -0,0 +1,28 @@
+const HeroSection = () => {
+
+    return <main className="Hero container">
+        <div className="Hero-content">
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+ <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
+    YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
+ </p>
+  <div className="Hero-button">
+            <button>Shop Now</button>
+            <button className="Second-btn">Catagory</button>
+        </div>
+
+        <div className="Shopping">
+            <p>Also Available On</p>
+            < img src="/images/amazon.png" alt=""/>
+            < img src="/images/flipkart.png" alt=""/>
+        </div>
+        </div>
+      
+        <div className="Hero-image">
+            <img src = "/images/shoe_image.png" alt=""/>
+        </div>
+
+    </main>
+}
+
+export default HeroSection;
---
+++ Navigation.jsx
@@ -0,0 +1,19 @@
+const Navigation = () => {
+    return(
+        
+          <nav className="container">
+    <div className="logo">
+      <img src ="/images/brand_logo.png" alt="brandlogo"/>
+    </div>
+    <ul>
+      <li herf="#">Menu</li>
+      <li herf="#">Contact</li>
+      <li herf="#">Location</li>
+      <li herf="#">About</li>
+    </ul>
+    <button>Login</button>
+  </nav>
+
+    )
+}
+export default Navigation;
